65ea858f41016f2a4fe9512062c5a59702296470
gricar <gabriel.rodrigues.ricardo@outlook.com>
2022-12-22 19:47:05 -0300

    code changes - automatically deploy

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          My first deploy with AWS Amplify
+          My first deploy with AWS Amplify !
         </p>
         <a
           className="App-link"
